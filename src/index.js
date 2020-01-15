df8c1bc50f4cfea16046b329a7cc88da4ee2d677
Patryk Dagiel <patryk.dagiel@gmail.com>
2020-01-15 20:41:33 +0100

    After 29th.

--- src/index.js
+++ src/index.js
@@ -4,19 +4,24 @@ import ReactDOM from 'react-dom';
 
 // Create a react component
 const App = () => {
-
-  const buttonText = {text: 'Click me!'};
-  const style = {backgroundColor: 'blue', color: 'white'};
-
-  return (
-  <div>
-    <label class = "label" for="name">Enter name:</label>
-    <input id="name" type="text" />
-    <button style={style}>
-      {buttonText.text}
-    </button>
-  </div>
-);
+  return(
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 6:00PM</span>
+          </div>
+          <div className="text">Some text!</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 // Take the react component and show it on the screen
