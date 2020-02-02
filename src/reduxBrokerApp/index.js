4eea2b08212b3ee78ccb2a10147cd9f53d5d7a3f
Patryk Dagiel <patryk.dagiel@gmail.com>
2020-02-02 18:20:27 +0100

    After 138.

--- src/reduxBrokerApp/index.js
+++ src/reduxBrokerApp/index.js
@@ -1,6 +1,6 @@
 console.clear()
 
-const createPolicy() => {
+const createPolicy = () => {
   return {  // Action (analogy, creating brokerage policy)
     type: 'CREATE_POLICY',
     payload: {
@@ -11,7 +11,7 @@ const createPolicy() => {
   };
 };
 
-const deletePolicy(name) => {
+const deletePolicy = (name) => {
   return {
     type: 'DELETE_POLICY',
     payload: {
@@ -29,3 +29,25 @@ const createClaim = (name, amountOfMoneyToCollect) => {
     }
   };
 };
+
+// Reducers [Deps]
+
+const claimsHistory = (oldListOfClaims = [], action) => {
+  if (action.type === 'CREATE_CLAIM') {
+    // we care about this form
+    return [...oldListOfClaims, action.payload]
+  }
+  return oldListOfClaims;
+  // we do not care about the form
+};
+
+const accounting = (bagOfMoney = 100, action) => {
+  if (action.type === 'CREATE_CLAIM') {
+    return bagOfMoney - action.payload.amountOfMoneyToCollect;
+  } else if (action.type === 'CREATE_POLICY') {
+    return bagOfMoney + action.payload.amount;
+  }
+  return bagOfMoney;
+};
+
+const
