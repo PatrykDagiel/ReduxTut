81970effe05835c470501aa19d2c4c491ea3e3c7
PatrykDagiel <patryk.dagiel@gmail.com>
2022-03-14 11:33:58 +0100

    React CC: After V92

--- prod/pics/src/components/SearchBar.js
+++ prod/pics/src/components/SearchBar.js
@@ -4,15 +4,21 @@ class SearchBar extends React.Component {
 
   state = { term: ''};
 
+  onFormSubmit = (event) => {
+    event.preventDefault(event);
+    console.log(this.state.term);
+  }
+
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field>">
             <label>Image Search</label>
-            <input type="text"
-            value={this.state.term}
-            onChange={(e) => this.setState({ term: e.target.value })}
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={(e) => this.setState({ term: e.target.value })}
             />
           </div>
         </form>
