85548de5bc9084d0ed4690b372b4fa5dd53b61b5
PatrykDagiel <patryk.dagiel@gmail.com>
2022-03-23 16:08:19 +0100

    React CC: After V111

---
+++ prod/pics/src/components/ImageCard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class ImageCard extends React.Component {
+  render() {
+    const { description, urls } = this.props.images;
+
+    return (
+      <div>
+        <img
+          alt={this.props.image.description}
+          src={this.props.image.urls.regular}
+        />
+      </div>
+    );
+  }
+}
+
+export default ImageCard;
